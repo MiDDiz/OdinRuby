70997e0b6375ea6144ec6ef9fb668c5c7724b423
MiDDiz <xxmidone30xx@gmail.com>
2020-07-25 13:21:49 +0200

    Changed a comment in stock_picker.rb

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,22 +1,23 @@
 def stock_picker stock_prices
-    #This vars hold the maximun relatives values and places for the stocks.
+    # This vars hold the maximun relatives values and places for the stocks.
     best_value = 0
     max_index = [0,0]
   
-    #As we need to get not only the maximum profit but the days we need to buy/sell, we keep track of the indexes as variables that will mutate as the loop continues. 
-    #my_index will hold the index of the stock that we are comparing with the others.
+    # As we need to get not only the maximum profit but the days we need to buy/sell, we keep track of the indexes as variables that will mutate as the loop continues. 
+    # my_index will hold the index of the stock that we are comparing with the others. 
+    # Array#index[value] cannot be used here because will cause undefinde behaviour if there are repeated values.
     my_index = 0
   
     stock_prices.each do |my_stock|
-      #a_index will be the stock that we compare to my_stock in order to get the relative best_value.
+      # a_index will be the stock that we compare to my_stock in order to get the relative best_value.
       a_index = 0
       stock_prices.each do |a_stock|
   
-        #We cant sell something before we buy it so if the relative index is lower than the index of the stock we are comparing then we have to pass. 
+        # We cant sell something before we buy it so if the relative index is lower than the index of the stock we are comparing then we have to pass. 
         if not a_index <= my_index
-          #Local variable in order to store the relativa value between stocks.
+          # Local variable in order to store the relativa value between stocks.
           my_value = a_stock - my_stock
-          #And if our relative value is more than our best value we switch them over and hold the new indexes as max_indexes.
+          # And if our relative value is more than our best value we switch them over and hold the new indexes as max_indexes.
           if my_value > best_value
             best_value = my_value
             max_index = [my_index, a_index]
